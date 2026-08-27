7ed5e749c6a1964737e04bf3ab282b63461e27ea
Martin Chen <martin.chen.au@gmail.com>
2026-08-27 17:40:35 +1000

    Hook issue fixes

--- fahhh.zsh
+++ fahhh.zsh
@@ -1,13 +1,37 @@
 autoload -Uz add-zsh-hook
 
+__fahhh_last_exit=0
+if (( ${+functions[precmd]} )) && [[ "${functions[precmd]}" != *__fahhh_last_exit* ]]; then
+	# Save original as __fahhh_orig_precmd if not already saved
+	if (( ! ${+functions[__fahhh_orig_precmd]} )); then
+		functions[__fahhh_orig_precmd]="${functions[precmd]}"
+	fi
+fi
+
+precmd() {
+	__fahhh_last_exit=$?
+	if (( ${+functions[__fahhh_orig_precmd]} )); then
+		__fahhh_orig_precmd "$@"
+	fi
+}
+
 __error_sound_precmd() {
-	local last_exit=$?
 	local dir="${0:A:h}"
 	local sounds=($dir/*.mp3(N))
-	if (( last_exit != 0 )) && (( ${#sounds} )); then
+	if (( __fahhh_last_exit != 0 )) && (( ${#sounds} )); then
 		local pick=$sounds[$(( RANDOM % ${#sounds} + 1 ))]
 		afplay "$pick" &>/dev/null &!
 	fi
 }
 
+__fahhh_save_exit() {
+	if (( __fahhh_last_exit == 0 )); then
+		local try=$?
+		(( try != 0 )) && __fahhh_last_exit=$try
+	fi
+}
+
+add-zsh-hook -D precmd __fahhh_save_exit 2>/dev/null
+add-zsh-hook -D precmd __error_sound_precmd 2>/dev/null
+add-zsh-hook precmd __fahhh_save_exit
 add-zsh-hook precmd __error_sound_precmd
