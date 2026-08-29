15fb6834e291e74d4e7a1993974d685940cb865f
Martin Chen <martin.chen.au@gmail.com>
2026-08-29 14:06:58 +1000

    Hook issue fixes

--- fahhh.zsh
+++ fahhh.zsh
@@ -1,37 +1,54 @@
 autoload -Uz add-zsh-hook
 
-__fahhh_last_exit=0
-if (( ${+functions[precmd]} )) && [[ "${functions[precmd]}" != *__fahhh_last_exit* ]]; then
-	# Save original as __fahhh_orig_precmd if not already saved
-	if (( ! ${+functions[__fahhh_orig_precmd]} )); then
-		functions[__fahhh_orig_precmd]="${functions[precmd]}"
+add-zsh-hook -D precmd __fahhh_save_exit 2>/dev/null
+add-zsh-hook -D precmd __error_sound_precmd 2>/dev/null
+add-zsh-hook -D precmd __fahhh_precmd 2>/dev/null
+if (( ${+functions[__fahhh_save_exit]} )); then
+	unfunction __fahhh_save_exit 2>/dev/null
+fi
+if (( ${+functions[__error_sound_precmd]} )); then
+	unfunction __error_sound_precmd 2>/dev/null
+fi
+if (( ${+functions[precmd]} )) && [[ "${functions[precmd]}" == *__fahhh_last_exit* ]]; then
+	if (( ${+functions[__fahhh_orig_precmd]} )); then
+		functions[precmd]="${functions[__fahhh_orig_precmd]}"
+		unfunction __fahhh_orig_precmd 2>/dev/null
+	else
+		unfunction precmd 2>/dev/null
 	fi
 fi
+if (( ${+functions[__fahhh_orig_precmd]} )); then
+	unfunction __fahhh_orig_precmd 2>/dev/null
+fi
+unset __fahhh_last_exit 2>/dev/null
 
-precmd() {
-	__fahhh_last_exit=$?
-	if (( ${+functions[__fahhh_orig_precmd]} )); then
-		__fahhh_orig_precmd "$@"
+typeset -g __fahhh_dir
+__fahhh_dir="${${(%):-%x}:A:h}"
+if [[ -z "$__fahhh_dir" || "$__fahhh_dir" == "." || ! -d "$__fahhh_dir" ]]; then
+	__fahhh_dir="${0:A:h}"
+fi
+if [[ -z "$__fahhh_dir" || ! -d "$__fahhh_dir" ]]; then
+	__fahhh_dir="${${funcfiletrace[1]%:*}:A:h}"
+fi
+
+__fahhh_precmd() {
+	local rc=$?
+	local dir=$__fahhh_dir
+	if [[ -z "$dir" || ! -d "$dir" ]]; then
+		dir="${${(%):-%x}:A:h}"
+		[[ -z "$dir" || ! -d "$dir" ]] && dir="${0:A:h}"
+		__fahhh_dir=$dir
 	fi
-}
-
-__error_sound_precmd() {
-	local dir="${0:A:h}"
 	local sounds=($dir/*.mp3(N))
-	if (( __fahhh_last_exit != 0 )) && (( ${#sounds} )); then
+	if (( rc != 0 )) && (( ${#sounds} )); then
 		local pick=$sounds[$(( RANDOM % ${#sounds} + 1 ))]
 		afplay "$pick" &>/dev/null &!
 	fi
+	return $rc
 }
 
-__fahhh_save_exit() {
-	if (( __fahhh_last_exit == 0 )); then
-		local try=$?
-		(( try != 0 )) && __fahhh_last_exit=$try
-	fi
-}
-
-add-zsh-hook -D precmd __fahhh_save_exit 2>/dev/null
-add-zsh-hook -D precmd __error_sound_precmd 2>/dev/null
-add-zsh-hook precmd __fahhh_save_exit
-add-zsh-hook precmd __error_sound_precmd
+add-zsh-hook -D precmd __fahhh_precmd 2>/dev/null
+typeset -ga precmd_functions 2>/dev/null
+if (( ! ${precmd_functions[(I)__fahhh_precmd]} )); then
+	precmd_functions=(__fahhh_precmd $precmd_functions)
+fi
